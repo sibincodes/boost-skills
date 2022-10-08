3147cf125d1b37e23b005968f37837bd2cf3d99e
Sibin John <sibinjohn@Sibins-MacBook-Air.local>
2022-10-09 00:46:09 +0530

    js initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+  const getResults = () => {
+    const mynum = [23,45,77,12];
+    const [a,b,c,d] = mynum;
+    const mynames={z:'sibin',x:'biju',y:'ramesh'};
+    const {y,...z} = mynames;
+    console.log(z);
+    return y;
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <div className='home'> {getResults()} </div>
+  )
 }
 
-export default App;
+export default App
